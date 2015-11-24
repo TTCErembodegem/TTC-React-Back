df7603387ca50474599583108f9e5fd80f9c453d
Laoujin <woutervs@hotmail.com>
2015-11-24 06:48:42 +0100

    ef6 works

---
+++ src/Ttc.WebApi/Controllers/ConfigController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Ttc.DataAccess;
+
+namespace Ttc.WebApi.Controllers
+{
+    public class ConfigController : ApiController
+    {
+        public string Get()
+        {
+            var x = new TtcDbContext();
+            return x.Spelers.First().Naam;
+        }
+    }
+}
---
+++ src/Ttc.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ttc.WebApi.WebApiApplication" Language="C#" %>
