0a35f1b7277c939a10b316ea59bbf3ecac5038bb
Laoujin <woutervs@hotmail.com>
2015-11-30 19:22:24 +0100

    added PlayersController

---
+++ src/Ttc.WebApi/Controllers/PlayersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Ttc.DataAccess;
+
+namespace Ttc.WebApi.Controllers
+{
+    public class PlayersController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            using (var x = new TtcDbContext())
+            {
+                return Json(x.Spelers);
+            }
+        }
+    }
+}
