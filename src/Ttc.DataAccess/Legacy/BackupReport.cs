47af27c9ce16c732d6b36f02010f486e0f92a58d
Laoujin <woutervs@hotmail.com>
2016-02-22 02:49:49 +0100

    two legit files got gitignored

---
+++ src/Ttc.DataAccess/Legacy/BackupReport.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ttc.DataAccess.Backup
+{
+    public class BackupReport
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FrenoyMatchId { get; set; }
+        public string Description { get; set; }
+        public int PlayerId { get; set; }
+
+        public BackupReport(string frenoyMatchId, string description, int playerId)
+        {
+            FrenoyMatchId = frenoyMatchId;  
+            Description = description;
+            PlayerId = playerId;
+        }
+
+        public override string ToString()
+        {
+            return $"FrenoyMatchId: {FrenoyMatchId}, Description: {Description}, PlayerId: {PlayerId}";
+        }
+    }
+}
---
+++ src/Ttc.DataAccess/Legacy/BackupTeamPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ttc.DataAccess.Backup
+{
+    public class BackupTeamPlayer
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public string DivisionLinkId { get; set; }
+        public string TeamCode { get; set; }
+
+        public override string ToString()
+        {
+            return $"PlayerId: {PlayerId}, DivisionLinkId: {DivisionLinkId}, TeamCode: {TeamCode}";
+        }
+    }
+}
