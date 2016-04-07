9e38ccef4263567990e1fec6d1b405b481940c85
Laoujin <woutervs@hotmail.com>
2016-04-07 21:24:52 +0200

    backend logging also perhaps...

--- src/Ttc.WebApi/Utilities/ExceptionFilter.cs
+++ src/Ttc.WebApi/Utilities/ExceptionFilter.cs
@@ -5,13 +5,18 @@ using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http.Filters;
+using log4net;
 
 namespace Ttc.WebApi.Utilities
 {
     public class TtcExceptionFilterAttribute: ExceptionFilterAttribute
     {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(ExceptionFilterAttribute));
+
         public override void OnException(HttpActionExecutedContext context)
         {
+            Logger.Error(context.Exception.ToString());
+
             context.Response = new HttpResponseMessage(HttpStatusCode.NotFound)
             {
                 Content = new StringContent(context.Exception.ToString()),
