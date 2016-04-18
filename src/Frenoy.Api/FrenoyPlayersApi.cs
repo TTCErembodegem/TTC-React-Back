705d96f6e4ccb56d81ff04f3dfff4248719e8c75
Laoujin <woutervs@hotmail.com>
2016-04-18 20:13:51 +0200

    WIP: Sync players with Frenoy

---
+++ src/Frenoy.Api/FrenoyPlayersApi.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Frenoy.Api.FrenoyVttl;
+using Ttc.DataEntities.Core;
+using Ttc.Model.Players;
+using Ttc.Model.Teams;
+
+namespace Frenoy.Api
+{
+    public class FrenoyPlayersApi : FrenoyApiBase
+    {
+        public FrenoyPlayersApi(ITtcDbContext ttcDbContext, Competition comp) : base(ttcDbContext, comp)
+        {
+
+        }
+
+        public void SyncPlayers()
+        {
+            var frenoyPlayers = _frenoy.GetMembers(new GetMembersRequest
+            {
+                Club = _settings.FrenoyClub
+            });
+
+            foreach (MemberEntryType frenoyPlayer in frenoyPlayers.MemberEntries)
+            {
+                var existingPlayer = _db.Players.SingleOrDefault(ply => ply.ComputerNummerVttl.HasValue && ply.ComputerNummerVttl.Value.ToString() == frenoyPlayer.UniqueIndex);
+                if (existingPlayer != null)
+                {
+                    //existingPlayer.IndexVttl
+                }
+
+
+            }
+        }
+    }
+}
