903aedab1d0ad60d492ef39fa06b78651bab52da
Laoujin <woutervs@hotmail.com>
2016-07-16 15:05:50 +0200

    Syncer: Seed: if endOfSeason, change the date of some matches

--- src/Frenoy.Syncer/Program.cs
+++ src/Frenoy.Syncer/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Frenoy.Api;
 using Ttc.DataAccess;
 using Ttc.DataAccess.Migrations;
@@ -13,6 +15,37 @@ namespace Frenoy.Syncer
             using (var context = new TtcDbContext())
             {
                 Configuration.Seed(context, true, true);
+
+                bool endOfSeason = !context.Matches.Any(match => match.Date > DateTime.Now);
+                if (endOfSeason)
+                {
+                    var passedMatches = context.Matches
+                        .Where(x => x.Date < DateTime.Today)
+                        .OrderByDescending(x => x.Date)
+                        .Take(42);
+
+                    var timeToAdd = DateTime.Today - passedMatches.First().Date;
+                    foreach (var match in passedMatches.Take(20))
+                    {
+                        match.Date = match.Date.Add(timeToAdd);
+                    }
+
+                    var rnd = new Random();
+                    foreach (var match in passedMatches.Take(20))
+                    {
+                        match.Date = DateTime.Today.Add(TimeSpan.FromDays(rnd.Next(1, 20))).AddHours(rnd.Next(10, 20));
+                        match.Description = "";
+                        match.AwayScore = null;
+                        match.HomeScore = null;
+                        //match.IsSyncedWithFrenoy = true;
+                        match.WalkOver = false;
+
+                        context.MatchComments.RemoveRange(match.Comments.ToArray());
+                        context.MatchGames.RemoveRange(match.Games.ToArray());
+                        context.MatchPlayers.RemoveRange(match.Players.ToArray());
+                    }
+                }
+
                 context.SaveChanges();
             }
         }
