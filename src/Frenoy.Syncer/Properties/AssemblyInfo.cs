256d49f931da401f60ec3a21fa23e1a1b197ba6f
Laoujin <woutervs@hotmail.com>
2017-09-10 23:53:20 +0200

    assemblyinfo update

--- src/Frenoy.Syncer/Properties/AssemblyInfo.cs
+++ src/Frenoy.Syncer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Frenoy.Syncer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © pongit 2015-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- src/Ttc.DataEntities/Properties/AssemblyInfo.cs
+++ src/Ttc.DataEntities/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Ttc.DataEntities")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © pongit 2015-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
