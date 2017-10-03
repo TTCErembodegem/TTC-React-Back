c55ffe8a9270a5520435b5c9abca6070fe1e5398
Laoujin <woutervs@hotmail.com>
2017-10-04 00:35:51 +0200

    ReadOnlyMatch: added ShouldBePlayed

--- src/Ttc.Model/Matches/Match.cs
+++ src/Ttc.Model/Matches/Match.cs
@@ -14,6 +14,7 @@ namespace Ttc.Model.Matches
         #region Properties
         public int Id { get; set; }
         public DateTime Date { get; set; }
+        public bool ShouldBePlayed { get; set; }
         public string FrenoyMatchId { get; set; }
         public bool IsSyncedWithFrenoy { get; set; }
         public int Week { get; set; }
